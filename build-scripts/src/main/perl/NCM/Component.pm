068bec1e9d68de09ae89344aae7b853f6f960ca4
Luis Fernando Muñoz Mejías <Luis.Munoz@UGent.be>
2012-07-10 15:37:25 +0200

    Enable tests on components that rely on ncm-template.

--- build-scripts/src/main/perl/NCM/Component.pm
+++ build-scripts/src/main/perl/NCM/Component.pm
@@ -12,10 +12,14 @@ package NCM::Component;
 use strict;
 use warnings;
 use parent qw(CAF::Object Exporter);
+use Template;
+use Template::Stash;
 
 our @EXPORT = qw($NoAction);
 
 our $NoAction;
+$Template::Stash::PRIVATE = undef;
+
 
 sub _initialize
 {
@@ -24,7 +28,18 @@ sub _initialize
     $self->{name} = $name;
     $self->{log} = $log || $main::this_app;
 
+    $self->{template} = Template->new(INCLUDE_PATH =>
+				      'target/share/templates/quattor',
+				      #DEBUG => 'undef'
+	);
+
     return 1;
+}
+
+sub template
+{
+    my $self = shift;
+    return $self->{template};
 }
 
 no strict 'refs';
