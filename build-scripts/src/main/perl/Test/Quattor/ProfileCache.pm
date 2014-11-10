77a080b2fa29a1e4fd9c1289af6f07e2559ff288
stdweird <stijn.deweirdt@ugent.be>
2014-11-10 11:03:24 +0100

    Cleanup ProfileCache code, add method to pass additional panc options

--- build-scripts/src/main/perl/Test/Quattor/ProfileCache.pm
+++ build-scripts/src/main/perl/Test/Quattor/ProfileCache.pm
@@ -6,6 +6,7 @@ package Test::Quattor::ProfileCache;
 use base 'Exporter';
 
 use CAF::FileWriter;
+use Test::More;
 
 use Cwd;
 use Carp qw(carp croak);
@@ -15,7 +16,14 @@ use EDG::WP4::CCM::Configuration;
 use EDG::WP4::CCM::CacheManager;
 use EDG::WP4::CCM::Fetch;
 
-our @EXPORT = qw(get_config_for_profile prepare_profile_cache);
+use Readonly;
+
+Readonly my $RELATIVE_RESOURCES => "src/test/resources";
+Readonly my $RELATIVE_PROFILES => "target/test/profiles";
+Readonly my $RELATIVE_CACHE => "target/test/cache";
+
+our @EXPORT = qw(get_config_for_profile prepare_profile_cache
+                 set_panc_options reset_panc_options);
 
 =pod
 
@@ -25,7 +33,85 @@ Module to setup a profile cache
 
 =cut
 
-my %configs;
+my (%configs, %pancoptions);
+
+=pod
+
+=head2 set_panc_options
+
+Set additional panc commandline options.
+Use the long option name, the preceding '--' is added.
+If no value is expected (e.g. '--debug') pass 'undef' as value.
+
+=cut
+
+sub set_panc_options
+{
+    my (%options) = @_;
+    while (my ($option, $value) = each %options) {
+        $pancoptions{$option} = $value;
+    }
+}
+
+
+=pod
+
+=head2 reset_panc_options
+
+Reset the panc commandline options.
+
+=cut
+
+sub reset_panc_options
+{
+    %pancoptions = ();
+}
+
+
+# get_panc_options returns hash reference to the additional pancoptions
+# test function only?
+sub get_panc_options
+{
+    return \%pancoptions;
+}
+
+=pod
+
+=head2 Compile pan object template into JSON profile
+
+Compile the pan C<profile> (file 'C<profile>.pan' in C<resourcesdir>)
+and create the profile in C<outputdir>.
+
+=cut
+
+sub panc
+{
+
+    my ($profile, $resourcesdir, $outputdir) = @_;
+
+    if( ! -d $outputdir) {
+        mkpath($outputdir) 
+            or croak("Couldn't create output directory $outputdir");
+    }
+    
+    my $currentdir = getcwd();
+    chdir($resourcesdir) or croak("Couldn't enter resources directory $resourcesdir");
+
+    my @panccmd = qw(panc --formats json --output-dir);
+    push(@panccmd, $outputdir);
+    while (my ($option,$value) = each %pancoptions) {
+        push(@panccmd, "--$option");
+        # support options like --debug with no value
+        push(@panccmd, $value) if (defined($value));
+    }        
+    push(@panccmd, "$profile.pan");
+    
+    note("Pan compiler called with: ".join(" ", @panccmd));
+    system(@panccmd) == 0
+        or croak("Unable to compile profile $profile");
+
+    chdir($currentdir);
+};
 
 =pod
 
@@ -41,7 +127,12 @@ sub prepare_profile_cache
 {
     my ($profile) = @_;
 
-    my $cache = "target/test/cache/$profile";
+    my $currentdir = getcwd();
+
+    my $cache = "$currentdir/$RELATIVE_CACHE/$profile";
+    my $profilesdir = "$currentdir/$RELATIVE_PROFILES";
+    my $resourcesdir = "$currentdir/$RELATIVE_RESOURCES";
+
     mkpath($cache);
 
     my $fh = CAF::FileWriter->new("$cache/global.lock");
@@ -54,22 +145,16 @@ sub prepare_profile_cache
     print $fh "1\n";
     $fh->close();
 
-    # Make output dir
-    mkpath("target/test/profiles");
 
-    my $d = getcwd();
+    # Compile profiles
+    panc($profile, $resourcesdir, $profilesdir);
 
-    chdir("src/test/resources") or croak("Couldn't enter resources directory");
-
-    system(qw(panc --formats json --output-dir ../../../target/test/profiles), "$profile.pan") == 0
-        or croak("Unable to compile profile $profile");
-
-    chdir($d);
+    # Setup CCM
     my $f = EDG::WP4::CCM::Fetch->new({
                FOREIGN => 0,
-               CONFIG => 'src/test/resources/ccm.cfg',
+               CONFIG => "$resourcesdir/ccm.cfg",
                CACHE_ROOT => $cache,
-               PROFILE_URL => "file://$d/target/test/profiles/$profile.json",
+               PROFILE_URL => "file://$profilesdir/$profile.json",
                })
         or croak ("Couldn't create fetch object");
     $f->{CACHE_ROOT} = $cache;
--- build-scripts/src/test/perl/profilecache.t
+++ build-scripts/src/test/perl/profilecache.t
@@ -3,10 +3,14 @@ use warnings;
 
 use Test::More;
 
-use Test::Quattor::ProfileCache qw(prepare_profile_cache get_config_for_profile);
+use Test::Quattor::ProfileCache qw(prepare_profile_cache get_config_for_profile set_panc_options reset_panc_options);
 
 # Can't have NoAction here, since no CAF mocking happens
 # and otherwise nothing would be written
+
+set_panc_options(debug => undef);
+
+is_deeply(Test::Quattor::ProfileCache::get_panc_options, {debug => undef}, "Options set");
 
 prepare_profile_cache('profilecache');
 
@@ -18,5 +22,9 @@ isa_ok($cfg, "EDG::WP4::CCM::Configuration",
 is_deeply($cfg->getElement("/")->getTree(), 
             {test => "data"}, 
             "getTree of root element returns correct hashref");
+
+reset_panc_options();
+
+is_deeply(Test::Quattor::ProfileCache::get_panc_options, {}, "Options reset");
 
 done_testing();
