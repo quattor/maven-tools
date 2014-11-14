7d86ec901be7e59f3533878194fc34d156a2f3d8
stdweird <stijn.deweirdt@ugent.be>
2014-11-14 16:45:08 +0100

    NCM::Component should reexport Test::Quattor::Component NoAction

--- build-scripts/src/main/perl/NCM/Component.pm
+++ build-scripts/src/main/perl/NCM/Component.pm
@@ -4,6 +4,9 @@ use warnings;
 package NCM::Component;
 
 # Correct the namespace for Test::Quattor::Component
-use base qw(Test::Quattor::Component);
+use base qw(Test::Quattor::Component Exporter);
+
+# reexport NoAction
+our @EXPORT = qw($NoAction);
 
 1;
